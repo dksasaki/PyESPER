1fdc4b17b938c4cab4f7628d43fb3d62958e9f02
LarissaMDias <126196089+LarissaMDias@users.noreply.github.com>
2025-07-07 11:44:49 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from disututils.core import setup
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description="Python ESPER",
+    author="LMD",
+    author_email="lmdias@uw.edu",
+    packages=["PyESPER"],
+    install_requires=["numpy", "seawater", "scipy", "matplotlib", "PyCO2SYS", "importlib"],
+)
