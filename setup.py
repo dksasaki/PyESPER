e92feb3dc25cd2322d4cbe378b264605971ce90b
dksasaki <dalton.sasaki@gmail.com>
2025-07-24 12:10:59 -0400

    bug correction in NeuralNetworks installation

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from distutils.core import setup
+import glob
+import os
+
+# Get all .py files in NeuralNetworks
+neural_net_files = glob.glob("NeuralNetworks/*.py")
 
 setup(
     name="PyESPER",
@@ -7,5 +12,8 @@ setup(
     author="LMD",
     author_email="lmdias@uw.edu",
     packages=["PyESPER"],
+    package_data={
+        'PyESPER': ['NeuralNetworks/*.py', 'NeuralNetworks/__init__.py']
+    },
     install_requires=["numpy", "seawater", "scipy", "matplotlib", "PyCO2SYS"],
 )
